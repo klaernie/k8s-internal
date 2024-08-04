44e5fa2c8d0319139c01fa41a7b1263ff5121089
Andre Klärner <kandre@ak-online.be>
2024-08-04 12:55:32 +0200

    make deploy.sh executable from everywhere, DWIM
    aka: when called with cwd in the top-level folder or from outside deploy
    all, and when called from a subdir only build the current working dir

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-cd "$(dirname "$0")"
+caller_dir=$( pwd -P )
+my_dir=$( cd "$(dirname "$0")"; pwd -P )
+if ! [[ "$caller_dir" = "$my_dir"* ]]; then
+	cd "$my_dir"
+fi
 kustomize build . --enable-helm | kubectl apply --server-side -f -
