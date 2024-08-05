7d6bcb039bc0c4f60d1e9f8cb1a4b2cafc38d609
Andre Klärner <kandre@ak-online.be>
2024-08-05 20:21:45 +0200

    deploy.sh: pass all additional arguments to kubectl apply

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ my_dir=$( cd "$(dirname "$0")"; pwd -P )
 if ! [[ "$caller_dir" = "$my_dir"* ]]; then
 	cd "$my_dir"
 fi
-kustomize build . --enable-helm | kubectl apply --server-side -f -
+kustomize build . --enable-helm | kubectl apply --server-side -f - "$@"
