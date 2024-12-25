99d33c7b9527062d4b303d6ff108d15b98301ca9
Andre Klärner <kandre@ak-online.be>
2024-12-25 23:36:34 +0100

    fail early when changing directory failed

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 caller_dir=$( pwd -P )
-my_dir=$( cd "$(dirname "$0")"; pwd -P )
+my_dir=$( cd "$(dirname "$0")" || exit 255; pwd -P ) || exit
 if ! [[ "$caller_dir" = "$my_dir"* ]]; then
-	cd "$my_dir"
+	cd "$my_dir" || exit 255
 fi
 kustomize build . --enable-helm | kubectl apply --server-side -f - "$@"
