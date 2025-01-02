4883633b079a9578eb6414f01e16f7819fafd29c
Andre Klärner <kandre@ak-online.be>
2025-01-02 14:20:04 +0100

    talos v1.9.0 broke the search domains, so we need to use fqdns until v1.9.1 which fixed it

--- _talos/upgrade.sh
+++ _talos/upgrade.sh
@@ -2,14 +2,14 @@
 set -x -e
 
 for host in \
-	talos-api01 \
-	talos-api02 \
-	talos-api03 \
-	talos-w01 \
-	talos-w02 \
-	talos-w03 \
-	talos-w04 \
-	talos-w05
+	talos-api01.ak-online.be. \
+	talos-api02.ak-online.be. \
+	talos-api03.ak-online.be. \
+	talos-w01.ak-online.be. \
+	talos-w02.ak-online.be. \
+	talos-w03.ak-online.be. \
+	talos-w04.ak-online.be. \
+	talos-w05.ak-online.be.
 do
 	talosctl upgrade --nodes $host --wait --image factory.talos.dev/installer/94114f5dbe67331c4f9b2be0eac92deb1575d3edd3cd2a59ec1731c2ad430d78:v1.8.4
 done
