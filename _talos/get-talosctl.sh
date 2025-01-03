d35dc93a72bc6d44285311186c37eade23bd7ab3
Andre Klärner <kandre@ak-online.be>
2025-01-03 14:33:50 +0100

    also script getting the newest talosctl

---
+++ _talos/get-talosctl.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+# renovate: datasource=github-releases depName=talos packageName=siderolabs/talos
+talosVersion=1.8.4
+
+wget https://github.com/siderolabs/talos/releases/download/v$talosVersion/talosctl-linux-amd64 -O "$HOME/bin/installs/talosctl-$talosVersion"
+chmod +x "$HOME/bin/installs/talosctl-$talosVersion"
+ln -sf installs/talosctl-$talosVersion "$HOME/bin/talosctl"
+
+echo "installed talosctl version $talosVersion"
