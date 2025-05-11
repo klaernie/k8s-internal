cfa7069e95eba5d2e8d0967a94354ee7e39b09e0
Andre Klärner <kandre@ak-online.be>
2025-05-11 19:25:51 +0200

    talos: use first element explicitly

--- _talos/upgrade-k8s.sh
+++ _talos/upgrade-k8s.sh
@@ -2,4 +2,4 @@
 
 source "$(dirname "$0")/definitions.sh" || exit 254
 
-talosctl upgrade-k8s -n $cp_nodes -e $cp_nodes --to $kubernetesVersion
+talosctl upgrade-k8s -n ${cp_nodes[0]} -e ${cp_nodes[0]} --to $kubernetesVersion
