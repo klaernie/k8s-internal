5248d58a68476ec57b9c9ea16eb7e5c6a9e9c502
Andre Klärner <kandre@ak-online.be>
2025-05-29 17:26:10 +0200

    remove no longer useful data

--- ldap/used_to_build_02_schema.yaml/test-with-docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker run --detach --rm --name openldap \
-	--env LDAP_ADMIN_USERNAME=admin \
-	--env LDAP_ADMIN_PASSWORD=testpassword \
-	--env LDAP_EXTRA_SCHEMAS=openssh-openldap \
-	--env LDAP_ROOT=dc=ak-online,dc=be \
-	bitnami/openldap:latest
