392ebb076bf68cfa91751739c5b3fc1379a2ba65
Andre Klärner <kandre@ak-online.be>
2025-05-29 15:50:39 +0200

    run shfmt

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-caller_dir=$( pwd -P )
-my_dir=$( cd "$(dirname "$0")" || exit 255; pwd -P ) || exit
+caller_dir=$(pwd -P)
+my_dir=$(
+	cd "$(dirname "$0")" || exit 255
+	pwd -P
+) || exit
 if ! [[ "$caller_dir" = "$my_dir"* ]]; then
 	cd "$my_dir" || exit 255
 fi
