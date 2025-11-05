e149824c57f7e1a652066ab92818acb2eb5bad82
Andre Klärner <kandre@ak-online.be>
2025-11-05 21:25:45 +0100

    talos: upgrade to 1.10.6 and 1.33.3

--- _talos/definitions.sh
+++ _talos/definitions.sh
@@ -15,7 +15,7 @@ export worker_nodes=(
 )
 
 # renovate: datasource=github-releases depName=talos packageName=siderolabs/talos
-export talosVersion=1.10.1
+export talosVersion=1.10.6
 
 # renovate: datasource=github-releases depName=talos-k8s packageName=siderolabs/kubelet
-export kubernetesVersion=1.33.0
+export kubernetesVersion=1.33.3
