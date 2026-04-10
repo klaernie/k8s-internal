d8c0f97a536bb734e6a557f9b1a28e3f9e613f18
Andre Klärner <kandre@ak-online.be>
2026-04-10 14:05:15 +0200

    magicmirror: convert small mm config to split directory

---
+++ magicmirror/s-config/config.js
@@ -0,0 +1,157 @@
+var config = {
+	address: "0.0.0.0",
+	port: 80,
+	ipWhitelist: [],
+
+	language: "en",
+	timeFormat: 24,
+	units: "metric",
+	timezone: "Europe/Berlin",
+
+	modules: [
+		{
+			module: "alert",
+		},
+		{
+			module: "clock",
+			position: "top_left",
+			config:{
+				showWeek: true,
+				clockBold: true,
+			}
+		},
+		{
+			module: "weather",
+			position: "top_left",
+			config: {
+				type: "current",
+				weatherProvider: "openweathermap",
+				location: "Dresden, Germany",
+				locationID: "2935022",  //ID from http://bulk.openweathermap.org/sample/; unzip the gz file and find your city
+				apiKey: "${OWM_APIKEY}"
+			}
+		},
+		{
+			module: 'MMM-CalendarExt2',
+			config: {
+				rotateInterval: 60*1000,
+				deduplicateEventsOn: [
+					"startDate", "endDate", "title",
+				],
+				scenes:[
+					{
+						name: "DEFAULT",
+						views: ["current","upcoming"],
+					},
+				],
+				views:[
+					{
+						name: "current",
+						mode: "current",
+						position: "top_right",
+						maxDays: 1,
+						locale: "en",
+						hideOverflow: false,
+						filterPassedEvent: true,
+					},
+					{
+						name: "upcoming",
+						mode: "upcoming",
+						position: "top_right",
+						maxDays: 3,
+						locale: "en",
+						hideOverflow: false,
+						filterPassedEvent: true,
+						useEventTimeRelative: false,
+					},
+				],
+				calendars: [
+					{
+						name: "Anja",
+						url: "http://calserv/AnjaPrivate.ics",
+					},
+					{
+						name: "Andre",
+						url: "http://calserv/AndrePrivate.ics",
+					},
+					{
+						name: "Family",
+						url: "http://calserv/Family.ics",
+					},
+					{
+						name: "Tobi",
+						url: "http://calserv/TobiPrivate.ics",
+						filter: (event) => {
+							if (event.title == "Schule") {
+								return false
+							}
+							return true
+						},
+					},
+					{
+						name: "Tobi Shared",
+						url: "http://calserv/TobiShared.ics",
+					},
+					{
+						name: "Tobi Lernsax",
+						url: "http://calserv/Lernsax.ics",
+						filter: (event) => {
+							if (event.title.startsWith("11:")) {
+								return false
+							}
+							if (event.title.startsWith("10:")) {
+								return false
+							}
+							if (event.title.startsWith("9S")) {
+								return false
+							}
+							if (event.title.startsWith("9:")) {
+								return false
+							}
+							if (event.title.startsWith("8S")) {
+								return false
+							}
+							if (event.title.startsWith("8:")) {
+								return false
+							}
+							if (event.title.startsWith("7S")) {
+								return false
+							}
+							if (event.title.startsWith("7:")) {
+								return false
+							}
+							if (event.title.startsWith("6S")) {
+								return false
+							}
+							if (event.title.startsWith("6:")) {
+								return false
+							}
+							if (event.title.startsWith("5S")) {
+								return false
+							}
+							if (event.title.startsWith("5:")) {
+								return false
+							}
+							return true
+						},
+					},
+					{
+						name: "Tanzkalender",
+						url: "http://calserv/Tanzkalender.ics",
+					},
+				],
+			},
+		},
+		{
+			module: 'MMM-Cursor',
+			config: {
+				timeout: 2500
+			}
+		}
+	]
+
+};
+
+/*************** DO NOT EDIT THE LINE BELOW ***************/
+if (typeof module !== "undefined") {module.exports = config;}
+
