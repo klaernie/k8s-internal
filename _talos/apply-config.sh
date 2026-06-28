d24f45ab809c447978a9190ecfc6490a010e2feb
Andre Klärner <kandre@ak-online.be>
2026-06-28 21:32:40 +0200

    seems the inline patch does not work anymore

--- _talos/apply-config.sh
+++ _talos/apply-config.sh
@@ -4,8 +4,10 @@ set -x -e
 source "$(dirname "$0")/definitions.sh" || exit 254
 
 for host in "${cp_nodes[@]}"; do
-	talosctl apply-config --endpoints "$host" --nodes "$host" --file controlplane.yaml --config-patch "[{\"op\": \"add\", \"path\": \"/machine/network/hostname\", \"value\": \"$host\"}]"
+	talosctl apply-config --endpoints "$host" --nodes "$host" --file controlplane.yaml
+	#--config-patch "[{\"op\": \"add\", \"path\": \"/machine/network/hostname\", \"value\": \"$host\"}]"
 done
 for host in "${worker_nodes[@]}"; do
-	talosctl apply-config --endpoints "$host" --nodes "$host" --file worker.yaml --config-patch "[{\"op\": \"add\", \"path\": \"/machine/network/hostname\", \"value\": \"$host\"}]"
+	talosctl apply-config --endpoints "$host" --nodes "$host" --file worker.yaml
+	#--config-patch "[{\"op\": \"add\", \"path\": \"/machine/network/hostname\", \"value\": \"$host\"}]"
 done
