a70df2439381bc715f03ee4d2080839a233599c2
Andre Klärner <kandre@ak-online.be>
2026-06-28 21:36:54 +0200

    diff.sh: like deploy.sh, just kubectl diff

---
+++ diff.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+caller_dir=$(pwd -P)
+my_dir=$(
+	cd "$(dirname "$0")" || exit 255
+	pwd -P
+) || exit
+if ! [[ "$caller_dir" = "$my_dir"* ]]; then
+	cd "$my_dir" || exit 255
+fi
+kustomize build . --enable-helm | kubectl diff --server-side -f - "$@"
